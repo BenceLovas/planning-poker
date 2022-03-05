4e5c6d0bd2147664aa892d7d744dbab4d01a255d
Bence Lovas <bansoul1@gmail.com>
2022-03-05 15:42:57 +0100

    style main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import { Button } from '@nextui-org/react'
 import { v4 as uuid } from 'uuid'
 import { useRouter } from 'next/router'
+import { Row, Spacer } from '@nextui-org/react'
 
 const Home: NextPage = () => {
   const router = useRouter()
@@ -12,41 +13,46 @@ const Home: NextPage = () => {
         <title>Planning Poker</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main
+      <div
         style={{
-          width: '100%',
           height: '100%',
           display: 'flex',
           justifyContent: 'center',
-          flexDirection: 'column',
-          gap: '1rem',
+          alignItems: 'center',
         }}
       >
-        <Button
-          size="lg"
-          rounded
-          onClick={() => router.push(`/room/type/planning/id/${uuid()}`)}
-        >
-          Create Planning Room
-        </Button>
-        <Button
-          size="lg"
-          rounded
-          onClick={() => router.push(`/room/type/t-shirt/id/${uuid()}`)}
-        >
-          Create T-Shit Sizing Room
-        </Button>
-        <Button
-          size="lg"
-          rounded
-          onClick={() => router.push(`/room/type/confidence/id/${uuid()}`)}
-        >
-          Create Confidence Room
-        </Button>
-      </main>
-
-      <footer></footer>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <Row gap={1} justify="center" align="center">
+            <Button
+              size="lg"
+              rounded
+              onClick={() => router.push(`/room/type/planning/id/${uuid()}`)}
+            >
+              Create Planning Room
+            </Button>
+          </Row>
+          <Spacer y={1} />
+          <Row gap={1} justify="center" align="center">
+            <Button
+              size="lg"
+              rounded
+              onClick={() => router.push(`/room/type/t-shirt/id/${uuid()}`)}
+            >
+              Create T-Shit Sizing Room
+            </Button>
+          </Row>
+          <Spacer y={1} />
+          <Row gap={1} justify="center" align="center">
+            <Button
+              size="lg"
+              rounded
+              onClick={() => router.push(`/room/type/confidence/id/${uuid()}`)}
+            >
+              Create Confidence Room
+            </Button>
+          </Row>
+        </div>
+      </div>
     </>
   )
 }
---
+++ pages/room/type/[room-type]/id/[id]/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Room: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Planning Poker - Room</title>
+      </Head>
+      <h1>hello world</h1>
+    </>
+  )
+}
+
+export default Room
