f717e457277e5a0521adc5a21356df837efe93f9
Bence Lovas <bansoul1@gmail.com>
2022-03-05 16:16:21 +0100

    set up themeing

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,27 @@ import { Button } from '@nextui-org/react'
 import { v4 as uuid } from 'uuid'
 import { useRouter } from 'next/router'
 import { Row, Spacer } from '@nextui-org/react'
+import { useTheme as useNextTheme } from 'next-themes'
+import { Switch, useTheme } from '@nextui-org/react'
 
 const Home: NextPage = () => {
   const router = useRouter()
+  const { setTheme } = useNextTheme()
+  const { isDark, type } = useTheme()
+
   return (
     <>
       <Head>
         <title>Planning Poker</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div>
+        The current theme is: {type}
+        <Switch
+          checked={isDark}
+          onChange={(e) => setTheme(e.target.checked ? 'dark' : 'light')}
+        />
+      </div>
       <div
         style={{
           height: '100%',
@@ -26,6 +38,7 @@ const Home: NextPage = () => {
             <Button
               size="lg"
               rounded
+              shadow
               onClick={() => router.push(`/room/type/planning/id/${uuid()}`)}
             >
               Create Planning Room
@@ -36,6 +49,7 @@ const Home: NextPage = () => {
             <Button
               size="lg"
               rounded
+              shadow
               onClick={() => router.push(`/room/type/t-shirt/id/${uuid()}`)}
             >
               Create T-Shit Sizing Room
@@ -46,6 +60,7 @@ const Home: NextPage = () => {
             <Button
               size="lg"
               rounded
+              shadow
               onClick={() => router.push(`/room/type/confidence/id/${uuid()}`)}
             >
               Create Confidence Room
